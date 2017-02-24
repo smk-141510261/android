a85955c72b5bf6b7832dbc93c9eb952c073f5b0f
smk-141510261 <141510261@smkassalaambandung.sch.id>
2017-02-25 06:47:45 +0700

    First

---
+++ Android/app/src/main/java/assalaam/studio/ujikom/app/AppConfig.java
@@ -0,0 +1,9 @@
+package assalaam.studio.ujikom.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://ujikom-2017.smkassalaambandung.sch.id/api/login";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://ujikom-2017.smkassalaambandung.sch.id/api/register";
+}
